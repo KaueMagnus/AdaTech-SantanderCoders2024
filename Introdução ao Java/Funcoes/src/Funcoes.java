cc4c0c669ad3c9d1fbf252881405f05a01d73d38
KaueMagnus <kmarquesmagnus@gmail.com>
2024-06-12 21:44:44 -0300

    add: funcoes

---
+++ Introdução ao Java/Funcoes/src/Funcoes.java
@@ -0,0 +1,18 @@
+public class Funcoes {
+    public static void main(String[] args) {
+//        String nomeOriginal = "Let's Code!";
+//        saudacao(nomeOriginal);
+
+        int result = sum(2, 3);
+        System.out.println(result);
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+//    public static void saudacao(String nomeParametro) {
+//        System.out.println("Hello, " + nomeParametro);
+//    }
+}
