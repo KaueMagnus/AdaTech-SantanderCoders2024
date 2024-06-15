606823e64b3fb898b676b8c012481ea575bd0716
KaueMagnus <kmarquesmagnus@gmail.com>
2024-06-15 14:50:50 -0300

    add: modifiers, constructors and getters/setters

--- Programação Orientada a Objetos Básica/Classes-Nao-Executaveis/src/Main.java
+++ Programação Orientada a Objetos Básica/Classes-Nao-Executaveis/src/Main.java
@@ -4,17 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro("Puppy", "Marrom", 25, 7.8, 5, "nada");
 
-        cachorro1.nome = "Puppy";
-        cachorro1.cor = "Marrom";
-        cachorro1.altura = 50;
-        cachorro1.peso = 7.8;
-        cachorro1.tamanhoDoRabo = 5;
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getPeso());
 
-        cachorro1.latir();
-        System.out.println(cachorro1.pegar());
-
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
+        System.out.println("\nO cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
+        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
 
     }
 }
